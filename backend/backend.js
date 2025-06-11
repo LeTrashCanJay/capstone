afea1d7d1e36f83977f50fe8be481e3d7bb7d863
LeTrashCanJay <jj147@iu.edu>
2025-06-11 02:43:56 -0400

    well. code works

--- backend/backend.js
+++ backend/backend.js
@@ -1,4 +1,32 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql")
 
-app
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "phpuser",
+    password: "phpuser",
+    database: "privacy_marketplace"
+});
+
+connection.connect(function(err) {
+    if(err) {
+        console.log(err.code);
+        console.log(err.fatal);
+    }
+});
+
+$query = "SELECT * FROM users";
+
+connection.query($query, function(err, rows, fields) {
+    if(err) {
+        console.log("An error occure with the query");
+        return;
+    }
+
+    console.log("Query Successfully Executed.", rows);
+});
+
+connection.end(function(){
+    console.log("Connection closed.")
+});
